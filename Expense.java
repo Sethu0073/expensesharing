1104579c39e9c3d4ecb6ac4e0cb9c8cdfc539ae0
Sethu Kumaran <150825848+Sethu0073@users.noreply.github.com>
2026-03-14 11:41:34 +0530

    Add Expense class with attributes and constructor

--- Expense.java
+++ Expense.java
@@ -1,1 +1,30 @@
+package model;
 
+import java.util.List;
+
+public class Expense {
+
+    private String description;
+    private double amount;
+    private Member paidBy;
+    private List<Member> participants;
+
+    public Expense(String description, double amount, Member paidBy, List<Member> participants) {
+        this.description = description;
+        this.amount = amount;
+        this.paidBy = paidBy;
+        this.participants = participants;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Member getPaidBy() {
+        return paidBy;
+    }
+
+    public List<Member> getParticipants() {
+        return participants;
+    }
+}
